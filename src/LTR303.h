0b128b01a9c497abefb3e7bf05e0836c4a668162
CDFER <cdirksfer@gamil.com>
2024-04-23 11:28:39 +1200

    - reformat library with .clang - add get error text - refactor registers to enum - rewrite doxygen comments

--- src/LTR303.h
+++ src/LTR303.h
@@ -33,170 +33,173 @@ THE SOFTWARE.
 #include <Arduino.h>
 #include <Wire.h>
 
-#define LTR303_ADDR 0x29  // default i2c address
+// Default I2C address for the sensor
+#define LTR303_ADDR 0x29
 
-// LTR303 register addresses
-#define LTR303_CONTR 0x80
-#define LTR303_MEAS_RATE 0x85
-#define LTR303_PART_ID 0xA0
-#define LTR303_MANUFACTURER_ID 0x05
-#define LTR303_DATA_CH1_0 0x88
-#define LTR303_DATA_CH1_1 0x89
-#define LTR303_DATA_CH0_0 0x8A
-#define LTR303_DATA_CH0_1 0x8B
-#define LTR303_STATUS 0x8C
+// Register addresses for the sensor
+enum {
+    LTR303_CONTR = 0x80,
+    LTR303_MEAS_RATE = 0x85,
+    LTR303_PART_ID = 0xA0,
+    LTR303_MANUFACTURER_ID = 0x05,
+    LTR303_DATA_CH1_0 = 0x88,
+    LTR303_DATA_CH1_1 = 0x89,
+    LTR303_DATA_CH0_0 = 0x8A,
+    LTR303_DATA_CH0_1 = 0x8B,
+    LTR303_STATUS = 0x8C
+};
 
-// gain = 0  => 1X gain (default)
-// gain = 1  => 2X gain
-// gain = 2  => 4X gain
-// gain = 3  => 8X gain
-// gain = 4, invalid
-// gain = 5, invalid
-// gain = 6  => 48X gain
-// gain = 7  => 96X gain
+#define AUTO_GAIN_UNDEREXPOSED_THRESHOLD 655   // Determines if either sensor underexposed (655 = ~1% of max raw)
+#define AUTO_GAIN_OVEREXPOSED_THRESHOLD 58982  // Determines if either sensor overexposed (58982 = ~50 % of max raw)
+
+// Enum for gain settings (1X to 96X)
 enum ltr303Gain {
-	GAIN_1X = 0,
-	GAIN_2X = 1,
-	GAIN_4X = 2,
-	GAIN_8X = 3,
-	GAIN_48X = 6,
-	GAIN_96X = 7
+    GAIN_1X = 0,
+    GAIN_2X = 1,
+    GAIN_4X = 2,
+    GAIN_8X = 3,
+    GAIN_48X = 6,
+    GAIN_96X = 7
 };
 
-// Exposure = 0 => 100ms (default)
-// Exposure = 1 => 50ms
-// Exposure = 2 => 200ms
-// Exposure = 3 => 400ms
-// Exposure = 4 => 150ms
-// Exposure = 5 => 250ms
-// Exposure = 6 => 300ms
-// Exposure = 7 => 350ms
+// Enum for exposure settings (50ms to 400ms)
 enum ltr303Exposure {
-	EXPOSURE_50ms = 1,
-	EXPOSURE_100ms = 0,
-	EXPOSURE_150ms = 4,
-	EXPOSURE_200ms = 2,
-	EXPOSURE_250ms = 5,
-	EXPOSURE_300ms = 6,
-	EXPOSURE_350ms = 7,
-	EXPOSURE_400ms = 3
+    EXPOSURE_50ms = 1,
+    EXPOSURE_100ms = 0,
+    EXPOSURE_150ms = 4,
+    EXPOSURE_200ms = 2,
+    EXPOSURE_250ms = 5,
+    EXPOSURE_300ms = 6,
+    EXPOSURE_350ms = 7,
+    EXPOSURE_400ms = 3
 };
 
-// interval = 0 =>50ms
-// interval = 1 =>100ms
-// interval = 2 =>200ms
-// interval = 3 =>500ms (default)
-// interval = 4 =>1000ms
-// interval = 5 =>2000ms
-// interval = 6 =>2000ms
-// interval = 7 =>2000ms
+// Enum for measurement intervals (50ms, 100ms, etc.)
 enum ltr303Interval {
-	LTR303_50ms_INTERVAL = 0,
-	LTR303_100ms_INTERVAL = 1,
-	LTR303_200ms_INTERVAL = 2,
-	LTR303_500ms_INTERVAL = 3,
+    LTR303_50ms_INTERVAL = 0,
+    LTR303_100ms_INTERVAL = 1,
+    LTR303_200ms_INTERVAL = 2,
+    LTR303_500ms_INTERVAL = 3
 };
 
 class LTR303 {
-   public:
-	/**
-	 * initializes the sensor.
-	 *
-	 * @param gain (enum) how much to amplify the reading (GAIN_1X)
-	 * @param exposure (enum) how long to keep the "shutter" open (EXPOSURE_100ms)
-	 * @param enableAutoGain change gain when new data received
-	 * @param port Wire instance (e.g Wire or Wire1)
-	 * @param addr i2c address of sensor
-	 * @retval 0 success
-	 * @retval 1 i2c data too long to fit in transmit buffer
-	 * @retval 2 i2c received NACK on transmit of address
-	 * @retval 3 i2c received NACK on transmit of data
-	 * @retval 4 i2c other error
-	 * @retval 5 i2c timeout
-	 */
-	uint8_t begin(ltr303Gain gain, ltr303Exposure exposure, bool enableAutoGain, TwoWire &port = Wire, uint8_t addr = LTR303_ADDR);
+  public:
 
-	/**
-	 * puts the sensor into active mode
-	 *
-	 * @retval 0 success
-	 * @retval 1 i2c data too long to fit in transmit buffer
-	 * @retval 2 i2c received NACK on transmit of address
-	 * @retval 3 i2c received NACK on transmit of data
-	 * @retval 4 i2c other error
-	 * @retval 5 i2c timeout
-	 */
-	uint8_t startPeriodicMeasurement();
+    /**
+     * Initializes the LTR303 sensor with specified gain, exposure, and auto-gain settings.
+     *
+     * This function sets up the LTR303 sensor for measurement. It takes in parameters for gain, exposure time, and auto-gain settings.
+     * @param gain The gain setting for the sensor (GAIN_1X, GAIN_2X, etc.)
+     * @param exposure The exposure time for each ALs cycle
+     * @param enableAutoGain Whether to adjust the gain when new data is received
+     * @param port The I2C port to use (default: Wire)
+     * @return 0 if successful, or an error code otherwise
+     */
+    uint8_t begin(ltr303Gain gain, ltr303Exposure exposure, bool enableAutoGain, TwoWire &port = Wire, uint8_t addr = LTR303_ADDR);
 
-	/**
-	 * puts the sensor into idle mode
-	 *
-	 * @retval 0 success
-	 * @retval 1 i2c data too long to fit in transmit buffer
-	 * @retval 2 i2c received NACK on transmit of address
-	 * @retval 3 i2c received NACK on transmit of data
-	 * @retval 4 i2c other error
-	 * @retval 5 i2c timeout
-	 */
-	uint8_t endPeriodicMeasurement();
+    /**
+     * Starts periodic measurement mode on the LTR303 sensor.
+     *
+     * This function starts the LTR303 sensor in periodic measurement mode. It will continue to measure data until endPeriodicMeasurement is called.
+     * @return 0 if successful, or an error code otherwise
+     */
+    uint8_t startPeriodicMeasurement();
 
-	/**
-	 * checks for correct response, manufacturer id and part id.
-	 *
-	 * @param port Wire instance (e.g Wire or Wire1)
-	 * @param stream debug output pointer (e.g. &Serial)
-	 * @param addr i2c address of sensor
-	 * @returns true if device correctly connected, otherwise false
-	 */
-	bool isConnected(TwoWire &port = Wire, Stream *stream = &Serial, uint8_t addr = LTR303_ADDR);
+    /**
+     * Stops periodic measurement mode on the LTR303 sensor.
+     *
+     * This function stops the LTR303 sensor from measuring data and returns it to idle mode.
+     * @return 0 if successful, or an error code otherwise
+     */
+    uint8_t endPeriodicMeasurement();
 
-	/**
-	 * gets Raw light count from sensor (no gain or exposure compensation and no checking if data is valid)
-	 *
-	 * @param visibleAndIRraw where to send the visible and IR sensor data
-	 * @param IRraw where to send the IR sensor output data
-	 * @retval 0 success
-	 * @retval 1 i2c data too long to fit in transmit buffer
-	 * @retval 2 i2c received NACK on transmit of address
-	 * @retval 3 i2c received NACK on transmit of data
-	 * @retval 4 i2c other error
-	 * @retval 5 i2c timeout
-	 * @retval 6 no data received on request
-	 * @retval 7 no new data available
-	 */
-	uint8_t getData(uint16_t &visibleAndIRraw, uint16_t &IRraw);
+    /**
+     * Checks whether the device is correctly connected and returns a string describing any errors.
+     *
+     * This function checks the connection status of the LTR303 sensor. If there are any issues with the connection, it will return a descriptive error message.
+     * @param port The I2C port to use (default: Wire)
+     * @param stream The debug output stream (default: &Serial)
+     * @param addr The I2C address of the sensor (default: LTR303_ADDR)
+     * @return true if device correctly connected, otherwise false
+     */
+    bool isConnected(TwoWire &port = Wire, Stream *stream = &Serial, uint8_t addr = LTR303_ADDR);
 
-	/**
-	 * gets Raw, checks validity, adjusts for gain and exposure and scales to passes back a approximate lux
-	 *
-	 * @param lux where to send the output value
-	 * @returns true if data returned is valid, otherwise false
-	 */
-	bool getApproximateLux(double &lux);
+    /**
+     * Retrieves raw light count data from the LTR303 sensor without gain or exposure compensation.
+     *
+     * This function retrieves raw data from the LTR303 sensor. The raw data is not adjusted for gain or exposure settings.
+     * @param visibleAndIRraw The variable to store the visible and IR sensor data
+     * @param IRraw The variable to store the IR sensor output data
+     * @return 0 if successful, or an error code otherwise. If no new data is available, returns 7.
+     */
+    uint8_t getData(uint16_t &visibleAndIRraw, uint16_t &IRraw);
 
-   private:
-	uint8_t reset();
+    /**
+     * Retrieves raw light count data from the LTR303 sensor, checks validity, adjusts for gain and exposure, and scales to an approximate lux value.
+     *
+     * This function retrieves raw data from the LTR303 sensor, validates it, and then scales it to an approximate lux value. The exact calculation used is dependent on the gain and exposure settings specified in begin.
+     * @param lux The variable to store the output value
+     * @return true if the data returned is valid, otherwise false
+     */
+    bool getApproximateLux(double &lux);
 
-	bool newDataAvailable();
+    /**
+     * Converts an error code into descriptive text.
+     *
+     * This function takes an error code and returns a string describing what that error means. It can be used to display error messages to users.
+     * @param errorCode The error returned by function that you want to convert
+     * @return A pointer to a const char array containing the descriptive text of the error. If the error code is not recognized, "Unknown error" is returned.
+     * @note The char array will never be longer than 48 characters
+     */
+    const char *getErrorText(uint8_t errorCode);
 
-	// Sets the gain, SW reset and mode of LTR303
-	uint8_t setControlRegister(bool reset = false, bool mode = true);
+  private:
+    // Resets the sensor to its default state
+    uint8_t reset();
 
-	bool autoGain(uint16_t visibleAndIRraw);
-	uint8_t setExposureTime();
+    // Checks if new data is available for retrieval
+    bool newDataAvailable();
 
-	uint8_t read16bitInt(uint8_t address, uint16_t &value);
+    // Sets the gain, reset register, and mode of the LTR303
+    uint8_t setControlRegister(bool reset = false, bool mode = true);
 
-	uint8_t _i2c_address;
-	TwoWire *_i2cPort;
-	Stream *_debug_output_stream = &Serial;
-	uint8_t _error;
-	bool _dataValid;
-	ltr303Gain _gain;
-	double _gainCompensation;
-	ltr303Exposure _exposure;
-	double _exposureCompensation;
-	bool _autoGainEnabled;
+    // Adjusts the gain based on sensor readings (if auto-gain is enabled)
+    bool autoGain(uint16_t visibleAndIRraw);
+
+    // Sets the exposure time for each ALs cycle and measurement interval
+    uint8_t setExposureTime();
+
+    // Reads a 16-bit integer from a specified address
+    uint8_t read16bitInt(uint8_t address, uint16_t &value);
+
+    // The I2C address of the LTR303 sensor.
+    uint8_t _i2c_address;
+
+    // The I2C port used to communicate with the LTR303 sensor.
+    TwoWire *_i2cPort;
+
+    // The debug output stream, defaulting to Serial if not specified.
+    Stream *_debug_output_stream = &Serial;
+
+    // An error code returned by various functions in the class.
+    uint8_t _error;
+
+    // A flag indicating whether new data is available from the LTR303 sensor.
+    bool _dataValid;
+
+    // The current gain setting for the LTR303 sensor.
+    ltr303Gain _gain;
+
+    // The compensation factor for the gain of the LTR303 sensor.
+    double _gainCompensation;
+
+    // The current exposure time set for the LTR303 sensor.
+    ltr303Exposure _exposure;
+
+    // The compensation factor for the exposure time of the LTR303 sensor.
+    double _exposureCompensation;
+
+    // A flag indicating whether automatic gain adjustment is enabled for the LTR303 sensor.
+    bool _autoGainEnabled;
 };
-
 #endif
